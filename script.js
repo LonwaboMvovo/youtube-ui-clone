404c127be1017d122cc7a7c6bc3085a4851450c3
LonwaboMvovo <44nator@gmail.com>
2020-08-28 17:19:09 +0200

    added trending page

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Click to trending page:
 const trending = document.getElementById("trending");
 trending.addEventListener("click", () => {
     console.log("Hello");
     window.location.href = "trending.html";
+});
+
+// Click to home page:
+const home = document.getElementById("home");
+home.addEventListener("click", () => {
+    console.log("Hello");
+    window.location.href = "index.html";
 });
