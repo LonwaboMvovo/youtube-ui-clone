c28de771388ffa840ebe4bd33efa68dcb6d1ff60
LonwaboMvovo <44nator@gmail.com>
2020-08-28 17:31:53 +0200

    added video page and functionality to click on a video to open the video page

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // Click to trending page:
 const trending = document.getElementById("trending");
 trending.addEventListener("click", () => {
-    console.log("Hello");
     window.location.href = "trending.html";
 });
 
 // Click to home page:
 const home = document.getElementById("home");
 home.addEventListener("click", () => {
-    console.log("Hello");
     window.location.href = "index.html";
 });
+
+// Click to video page:
+const trending_videos = document.getElementsByClassName("trending-video");
+const trending_vids = Array.from(trending_videos);
+trending_vids.forEach(myFunction);
+function myFunction(item, index) {
+    trending_vids[index].addEventListener("click", () => {
+        window.location.href = "video.html";
+    })
+}
